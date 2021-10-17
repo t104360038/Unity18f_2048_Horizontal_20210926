1a11c44fa276837eb11a30500752c28c75bce0a8
leo881116 <90996457+leo881116@users.noreply.github.com>
2021-10-17 13:04:05 +0800

    練習C#

---
+++ 2048_Horizontal_20210926/Assets/Scripts/Car.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //基礎資料類別    整數int 浮點數float  字串string    字元Char  布林bool
+    int CC = 2000;
+    float weight = 3.5f;
+    string brand = "賓士";
+    char CarDoor = '4';
+    bool haveCoorWindow = true;
+}
